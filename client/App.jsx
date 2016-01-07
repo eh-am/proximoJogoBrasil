7159fe6dfb56261bb079d1a42c0034d46b2b343c
Eduardo Aleixo <eduardoaleixomartins@gmail.com>
2016-01-06 23:36:30 -0500

    creates an empty opponent before fetching data

--- client/App.jsx
+++ client/App.jsx
@@ -1,22 +1,49 @@
 App = React.createClass({
+  getInitialState(){
+    // sets a dumb game
+    var nextGame = {
+      opponent: '?????',
+      isoName: 'br',
+      date: new Date()
+      // date: new Date(1458856800 * 1000)
+    };
+    return {
+      nextGame: nextGame
+    };
+  },
+
+  componentDidMount(){
+    // get data from amazon lambda
+
+    // $.ajax({
+    //   context: this,
+    //   url: 'https://toroprxmgj.execute-api.us-west-2.amazonaws.com/prod/getbrazilsnextgame',
+    //   type: 'GET',
+    //   crossDomain: true,
+    //   contentType: 'application/json',
+    //   success: function (result){
+    //     console.log('this is the result');
+    //     console.log(result);
+    //     this.setState({
+    //       nextOpponent: {
+    //         opponent: result.opponent.ptbr,
+    //         flag: this._getFlagUrl(result.opponent.iso),
+    //         date: new Date(result.time * 1000)
+    //       }
+    //     });
+    //   }
+    // });
+  },
+
+
   render(){
-    var nextGames = [{
-      opponent: 'Argentina',
-      flag: 'http://flagpedia.net/data/flags/normal/uy.png',
-      date: new Date(1458856800 * 1000)
-    }];
-
-    var nextOpponent = nextGames[0];
-
     return (
       <div className="container">
-        
-
         <NextGame
-          opponent={ nextOpponent.opponent }
-          flag={ nextOpponent.flag }
-          date= { nextOpponent.date }
-          />
+          opponent={ this.state.nextGame.opponent }
+          isoName={ this.state.nextGame.isoName }
+          date= { this.state.nextGame.date }
+        />
       </div>
     );
   }
--- client/components/NextGame/NextGame.jsx
+++ client/components/NextGame/NextGame.jsx
@@ -1,14 +1,20 @@
 // import moment from 'moment';
 
 NextGame = class NextGame extends React.Component{
+  _getFlagUrl(){
+    // return 'http://flagpedia.net/data/flags/normal/' + this.props.isoName.toLowerCase() + '.png';
+    return '';
+  }
+
+
   render(){
-    // moment.locale('pt-br');
+    moment.locale('pt-br');
     var m = moment(this.props.date);
     var date = m.format('LLL');
 
     return (
       <div className="nextGame">
-        <img src={this.props.flag} alt={this.props.opponent} className="flag"/>
+        <img src={this._getFlagUrl()} alt={this.props.opponent} className="flag"/>
         <h2 className="title"> { this.props.opponent } </h2>
         <h4 className="date">Em { date }</h4>
       </div>
